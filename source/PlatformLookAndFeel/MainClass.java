02dc9b28dd9e3b30de500447140d22d8c3668efb
Hordur Thor Jonsson <htjonsson@gmail.com>
2024-02-18 18:02:55 +0000

    Look&Feel test

---
+++ source/PlatformLookAndFeel/MainClass.java
@@ -0,0 +1,12 @@
+// Java sample code to get the list of 
+// installed Look and Feel themes, here is a sample code: 
+import javax.swing.UIManager; 
+
+public class MainClass { public static void main(String[] a) 
+	{ 
+		UIManager.LookAndFeelInfo[] looks = UIManager.getInstalledLookAndFeels(); 
+		for (UIManager.LookAndFeelInfo look : looks) { 
+			System.out.println(look.getClassName()); 
+		} 
+	} 
+} 
